61437f21ed3f51af40ef399d19d8b38058d46a83
Maks Mishin <maks.mishinFZ@gmail.com>
2023-05-31 00:34:08 +0300

    Change command to create superuser

--- users/management/commands/create_superuser.py
+++ users/management/commands/create_superuser.py
@@ -7,13 +7,8 @@ from users.models import User
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument('username', type=str, help='Username')
-        parser.add_argument('password', type=str, help='Password')
-
     def handle(self, *args, **options):
-        username = options['username']
-        password = options['password']
-
-        User.objects.create_superuser(username=username, password=password)
-        self.stdout.write(self.style.SUCCESS("Superuser created successfully"))
+        if User.objects.filter(username='admin')[0]:
+            self.stdout.write(self.style.SUCCESS("Superuser 'admin' already created"))
+        else:
+            User.objects.create_superuser(username='admin', password='admin')
